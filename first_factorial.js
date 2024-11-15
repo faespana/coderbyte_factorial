5279c685c84f4f1f91efa65c0954d8cd7fedf271
Desarrollador <desarrollador@iroute.local>
2024-11-15 08:58:09 -0500

    first commit

---
+++ first_factorial.js
@@ -0,0 +1,16 @@
+function FirstFactorial(num) { 
+
+    let result = 1
+
+while (num>0) {
+    result = result * num 
+    num = num -1 
+}
+
+    return result; 
+  }
+     
+  
+  console.log(FirstFactorial(5
+    
+  ));
